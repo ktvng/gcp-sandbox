048e8394f80581ec12e62e4ad6210d1e19ec6fe0
ktvng <35818433+ktvng@users.noreply.github.com>
2024-07-24 08:36:11 -0700

    bugfix

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ simple_http_function_service = importlib.import_module("simple-http-function.ent
 def simple_http_function(args):
     return simple_http_function_service.entry(args)
 
-secondary_function_service = importlib.import_module("secondary-function")
+secondary_function_service = importlib.import_module("secondary-function.entry")
 def secondary_function(args):
     return secondary_function_service.entry(args)
